429996063b96ead2aa6ed792c6da13ff423144eb
MACVLX <miguelvieira21@gmail.com>
2024-06-20 17:41:11 +0100

    GG

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ st.title("DENTUXO")
 
 st.write(read_query('MATCH (cat:Treatment {code: $code}) RETURN cat',params={'code':'HBGD475'}))
 
-# st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
-st.markdown("![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNG9uMm5kOWVrbnpjZHF3NXNjNGNvYnozbGhuOXJtdHBnOGF0OTZmNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qmoggo2eQMYY8/giphy.gif")
+st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
+# st.markdown("![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNG9uMm5kOWVrbnpjZHF3NXNjNGNvYnozbGhuOXJtdHBnOGF0OTZmNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qmoggo2eQMYY8/giphy.gif")
